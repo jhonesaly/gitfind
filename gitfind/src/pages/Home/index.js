64f3d476a026341a3b8adc6be6e0b5e68713e95d
Jhones <jhonesaly@gmail.com>
2023-11-09 17:12:32 -0300

    feat: requisição para a API

--- gitfind/src/pages/Home/index.js
+++ gitfind/src/pages/Home/index.js
@@ -1,9 +1,21 @@
-import {Header} from "../../components/Header";
+import {useState} from "react";
+import { Header } from "../../components/Header";
 import background from "../../assets/background.png";
 import "./styles.css";
 import ItemList from "../../components/ItemList";
 
 function App() {
+  const [user, setUser] = useState('');
+  const [currentUser, setCurrentUser] = useState(null);
+  const [repos, setRepos] = useState(null);
+
+  const handleGetData = async () => {
+    const userData = await fetch(`https://api.github.com/users/${user}`);
+    const newUser = await userData.json();
+
+    console.log(newUser);
+  }
+
   return (
     <div className="App">
       <Header />
@@ -11,8 +23,13 @@ function App() {
         <img src={background} className="background" alt="imagem de fundo"/>
         <div className="info">
           <div>
-            <input name="usuario" placeholder="@username"/>
-            <button>Buscar</button>
+            <input 
+              name="usuario" 
+              value={user} 
+              onChange={(event) => setUser(event.target.value)} 
+              placeholder="@username"
+            />
+            <button onClick={handleGetData}>Buscar</button>
           </div>
           <div className="perfil">
             <img 
