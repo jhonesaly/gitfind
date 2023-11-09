255615345c7aa0cc5dbd38b05d56332cbaf1dffc
Jhones <jhonesaly@gmail.com>
2023-11-09 11:59:32 -0300

    feat: fundo

--- gitfind/src/pages/Home/index.js
+++ gitfind/src/pages/Home/index.js
@@ -1,8 +1,14 @@
 import {Header} from "../../components/Header";
+import background from "../../assets/background.png";
+import "./styles.css";
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="conteudo">
+        <img src={background} className="background" alt="background app"/>
+      </div>
     </div>
   );
 }
