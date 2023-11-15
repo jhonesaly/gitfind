4abf15eff16ba207c7970c64b34368097e1fa316
Jhones <jhonesaly@gmail.com>
2023-11-15 12:04:13 -0300

    feat: melhora visual

--- gitfind/src/components/ItemList/index.js
+++ gitfind/src/components/ItemList/index.js
@@ -1,16 +1,15 @@
-// src/components/ItemList/index.jsx
 import React from 'react';
 import "./styles.css";
 
 function ItemList({ title, items }) {
   return (
-    <div>
+    <div className="item-list">
       <h4 className="list-title">{title}</h4>
       {items.map(item => (
-        <div key={item.id} className="item-list">
+        <div key={item.id} className="item">
+          <hr />
           <strong>{item.name}</strong>
           <p>{item.description}</p>
-          <hr />
         </div>
       ))}
     </div>
